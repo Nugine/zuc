11e8f7c0370682397771f456eac0e5e8cea1bce3
Nugine <nugine@foxmail.com>
2025-01-05 14:27:22 +0800

    refactor: move out utils functions

--- src/utils.rs
+++ src/utils.rs
@@ -2,18 +2,6 @@
 
 use std::ops::ShlAssign;
 
-/// (a + b) mod (2^32)
-#[inline(always)]
-pub fn add(a: u32, b: u32) -> u32 {
-    a.wrapping_add(b)
-}
-
-/// rotate left
-#[inline(always)]
-pub fn rol(x: u32, n: u32) -> u32 {
-    x.rotate_left(n)
-}
-
 /// 256-bit unsigned integer
 pub struct U256 {
     /// high 128 bits
